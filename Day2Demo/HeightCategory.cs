d5d10e2434f960f44592f3a5a669374d55d6c10f
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2025-12-23 09:57:35 +0530

    Add files via upload

---
+++ Day2Demo/HeightCategory.cs
@@ -0,0 +1,30 @@
+// using System;
+
+// public class HeightCategory
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("Enter height:");
+
+//         int height = int.Parse(Console.ReadLine());
+
+//         if (height < 150)
+//         {
+//             Console.WriteLine("Dwarf");
+//         }
+//         else if (height >= 150 && height < 165)
+//         {
+//             Console.WriteLine("Average");
+//         }
+//         else if (height >= 165 && height < 190)
+//         {
+//             Console.WriteLine("Taller");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Abnormal");
+//         }
+//     }
+// }
+
+
