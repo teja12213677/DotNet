ba417dd5c0f1d5c8397995a76b49dabcf29cfe1d
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-01-25 23:05:30 +0530

    Add files via upload

---
+++ TopBrainsAssessment/AreaOfCircle.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class AreaOfCircle{
+    public static double Calculate(double radius){
+        double area=Math.PI*radius*radius;
+        area=Math.Round(area,2,MidpointRounding.AwayFromZero);
+        return area;
+    }
+    
+    public static void Main(){
+        double r=5;
+        double result=Calculate(r);
+        Console.WriteLine(result);
+        
+        
+    }
+}
