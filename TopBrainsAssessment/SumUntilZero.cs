762afd3915d473fdfab062d2a1428aac30bc2d8a
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-01-25 22:45:55 +0530

    Add files via upload

---
+++ TopBrainsAssessment/SumUntilZero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class SumUntilZero{
+    public static int Sum(int[] nums){
+        int sum=0;
+        foreach(int num in nums)
+        {
+            if(num==0)
+            
+            break;
+            if(num<0)
+            continue;
+            
+            sum=sum+num;
+        }
+        return sum;
+        
+    }
+    public static void Main(){
+        int[] nums={5,-2,45,67,0,7};
+        
+        Console.WriteLine(Sum(nums));
+    }
+    
+    
+}
