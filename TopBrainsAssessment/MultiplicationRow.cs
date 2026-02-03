f40377c8f4fe01a85c7a303db45848fd290356e1
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-02-03 18:45:23 +0530

    Add files via upload

---
+++ TopBrainsAssessment/MultiplicationRow.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class MultiplicationRow
+{
+    static void Main()
+    {
+       
+        Console.Write("Enter n value: ");
+        
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter the value of upto: ");
+        int upto = int.Parse(Console.ReadLine());
+
+        int[] row = GetMultiplicationRow(n, upto);
+
+        Console.Write("[");
+        for (int i = 0; i < row.Length; i++)
+        {
+            Console.Write(row[i]);
+            if (i < row.Length - 1)
+                Console.Write(",");
+        }
+        Console.Write("]");
+    }
+
+    static int[] GetMultiplicationRow(int n, int upto)
+    {
+        int[] result = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            result[i - 1] = n * i;
+        }
+
+        return result;
+    }
+}
