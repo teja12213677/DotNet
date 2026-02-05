fe7ce8ea87ea8563f707146d380c16f40b4c0855
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-02-05 18:57:33 +0530

    Add files via upload

---
+++ TopBrainsAssessment/Fleet.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class HelloWorld
+{
+  
+        public static double FeetToCentimeter(int feet)
+        {
+        double centimeter=feet*30.48;
+        return Math.Round(centimeter, 2,MidpointRounding.AwayFromZero);
+        
+        }
+        
+    
+}
+class Fleet
+{
+    public static void Main(string[] args)
+    {
+       
+       
+        Console.WriteLine(HelloWorld.FeetToCentimeter(5).ToString("F2"));
+        Console.WriteLine(HelloWorld.FeetToCentimeter(1).ToString("F2"));
+
+    }
+}
