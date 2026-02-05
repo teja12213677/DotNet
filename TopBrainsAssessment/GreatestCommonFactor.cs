7f63cfb840af62cfdcac0db651a7ac006df27c72
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-02-05 19:18:32 +0530

    Add files via upload

---
+++ TopBrainsAssessment/GreatestCommonFactor.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Hello
+{
+    public static int GCD(int a, int b)
+    {
+        if(b==0)
+        return a;
+        
+        return GCD(b, a%b);
+    }
+}
+class GreatestCommonFactor
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Hello.GCD(45, 18));
+        Console.WriteLine(Hello.GCD(10,20));
+    }
+}
