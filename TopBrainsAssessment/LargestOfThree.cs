36130c7f330d15478edb0495456945d931aa73f7
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-02-05 19:29:48 +0530

    Add files via upload

---
+++ TopBrainsAssessment/LargestOfThree.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Hello
+{
+    public static int Highest(int a, int b, int c)
+    {
+        if(a>=b && a>=c)
+        return a;
+        else if(b>=a && b>=c)
+        return c;
+        else 
+        return c;
+        
+    }
+}
+class LargestOfThree
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Hello.Highest(5,6,7));
+        Console.WriteLine(Hello.Highest(23,34,65));
+    }
+}
